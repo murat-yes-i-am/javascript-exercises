c9f9f6200ea119d04dd33904e27739df17c7be47
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-01 12:19:15 +0300

    feat: implement leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const isDivisible = (divisor) => (num) => num % divisor === 0;
+const isDivisibleBy4 = isDivisible(4);
+const isDivisibleBy100 = isDivisible(100);
+const isDivisibleBy400 = isDivisible(400);
 
+const leapYears = function(year) {
+  return (isDivisibleBy4(year) && !isDivisibleBy100(year)) || isDivisibleBy400(year);
 };
 
 // Do not edit below this line
