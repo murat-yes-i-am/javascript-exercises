4aa4590741c045e2218617748f735ca7a65c4092
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-01 12:41:05 +0300

    feat: implement factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,19 @@ const power = function (base, exponent) {
   return base ** exponent;
 };
 
-const factorial = function () {
+const factorial = function (num, memo = {}) {
+  if (num < 2) {
+    return 1;
+  }
 
+  if (memo[num]) {
+    return memo[num]
+  } else {
+    const prev = factorial(num - 1, memo);
+    const result = prev * num;
+    memo[num] = result;
+    return result; 
+  }
 };
 
 // Do not edit below this line
