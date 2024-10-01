e1d33e39ed5b7596df586987a1d081f677c5de2e
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-01 12:04:29 +0300

    fix: add first number larger logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function(start, end) {
+const sumAll = function(num1, num2) {
   let sum = 0;
+
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
 
   sum = ((start + end) / 2) * (end - start + 1); // Gauss sum
 
