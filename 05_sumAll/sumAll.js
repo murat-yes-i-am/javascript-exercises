e2c60affeadcb6008a389701eb0aefd373495f45
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-01 12:10:21 +0300

    feat: add isInteger function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,8 @@
-const sumAll = function(num1, num2) {
+const sumAll = function (num1, num2) {
   if (num1 < 0 || num2 < 0) {
     return 'ERROR';
   }
-  
+
   let sum = 0;
 
   const start = Math.min(num1, num2);
@@ -12,6 +12,10 @@ const sumAll = function(num1, num2) {
 
   return sum;
 };
+
+const isInteger = (num) => {
+  return typeof num === 'number' && Math.floor(num) === num;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
