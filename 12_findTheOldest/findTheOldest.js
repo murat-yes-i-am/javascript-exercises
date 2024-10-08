6835236c33e1e7960e3f545c3d287929e84b00a9
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 12:07:42 +0300

    feat: implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  const ages = people.map(({yearOfBirth, yearOfDeath}) => (yearOfDeath ?? currentYear) - yearOfBirth);
+
+  const maxAge = Math.max(...ages);
+
+  const oldestIndex = ages.indexOf(maxAge);
+
+  // const oldestIndex = ages.reduce((prevMaxAgeIndex, age, index, array) => {
+  //   if (age > array[prevMaxAgeIndex]) { 
+  //     prevMaxAgeIndex = index;
+  //   }
+
+  //   return prevMaxAgeIndex;
+  // }, -1);
+
+  return people[oldestIndex];
 };
 
 // Do not edit below this line
