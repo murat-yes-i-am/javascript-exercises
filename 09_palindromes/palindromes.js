825da7533448988d345a5617be697d6a436ecb51
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:14:16 +0300

    feat: add isDigit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function () {
   const digits = Array(10).fill(1).map((value, index) => index);
   const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
+  const isDigit = (char) => digits.includes(char);
 };
 
 // Do not edit below this line
