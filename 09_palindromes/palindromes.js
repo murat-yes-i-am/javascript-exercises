b3b0689b7149d45e0a0dbd309867b5aae03a997c
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:30:39 +0300

    feat: add working with string of lower case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,15 @@
 const palindromes = function (string) {
+  const toLowerString = string.toLowerCase();
   const digits = Array(10).fill(1).map((value, index) => index);
   const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
   const isDigit = (char) => digits.includes(char);
   const isLetterDigit = (char) => isLetter(char) || isDigit(char);
-  const getLowerChar = (index, string) => string[index].toLowerCase();
+  const getChar = (index, string) => string[index];
 
   let left = 0;
   let right = string.length - 1;
-  let leftChar = getLowerChar(left, string);
-  let rightChar = getLowerChar(right, string);
+  let leftChar = getChar(left, toLowerString);
+  let rightChar = getChar(right, toLowerString);
 
   while (left < right) {
    
