762d6cff370342240a96ef54e44b178595aad15d
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:19:25 +0300

    feat: add isLetterDigit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,6 +2,7 @@ const palindromes = function (string) {
   const digits = Array(10).fill(1).map((value, index) => index);
   const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
   const isDigit = (char) => digits.includes(char);
+  const isLetterDigit = (char) => isLetter(char) || isDigit(char);
 
   let start = 0;
   let end = string.length - 1;
