b19a2f3ec98eb29c1906dabcd8f99c007991cdd0
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:17:08 +0300

    feat: add while loop and default return

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,6 +2,15 @@ const palindromes = function (string) {
   const digits = Array(10).fill(1).map((value, index) => index);
   const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
   const isDigit = (char) => digits.includes(char);
+
+  let start = 0;
+  let end = string.length - 1;
+
+  while (start < end) {
+
+  }
+
+  return true;
 };
 
 // Do not edit below this line
