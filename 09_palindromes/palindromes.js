3196a13eb37cff05460620152224a72b618c8a5a
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:13:47 +0300

    feat: add isLetter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
 const palindromes = function () {
   const digits = Array(10).fill(1).map((value, index) => index);
+  const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
 };
 
 // Do not edit below this line
