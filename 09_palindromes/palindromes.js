26452ba7c1db5abd3bec9eac77df0bc80295a9dc
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 10:28:56 +0300

    feat: add getLowerChar

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,12 +3,15 @@ const palindromes = function (string) {
   const isLetter = (char) => char.toUpperCase() !== char.toLowerCase();
   const isDigit = (char) => digits.includes(char);
   const isLetterDigit = (char) => isLetter(char) || isDigit(char);
+  const getLowerChar = (index, string) => string[index].toLowerCase();
 
   let left = 0;
   let right = string.length - 1;
+  let leftChar = getLowerChar(left, string);
+  let rightChar = getLowerChar(right, string);
 
   while (left < right) {
-
+   
   }
 
   return true;
